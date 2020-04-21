305dacdc162b318a4e203deee125ee3227cb14f7
cesaralonso <cesar_alonso_m_g@hotmail.com>
2020-04-21 10:30:44 -0500

    bootstrap cap to latest external schematic run

--- src/cap-angular-schematic-sfcore/index.ts
+++ src/cap-angular-schematic-sfcore/index.ts
@@ -49,7 +49,7 @@ export default function (options: any): Rule {
 }
 
 function addBootstrapSchematic() {
-    return externalSchematic('cap-angular-schematic-bootstrap@latest', 'ng-add', { version: "4.0.0", skipWebpackPlugin: true });
+    return externalSchematic('cap-angular-schematic-bootstrap@0.0.10', 'ng-add', { version: "4.0.0", skipWebpackPlugin: true });
 }
 
 function addToEnvironments(options: any): Rule {
