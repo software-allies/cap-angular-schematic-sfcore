bbeb8016a4b35a76f8861ced098c57492611381b
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2021-06-12 17:28:11 -0500

    Group the html template and the scss in the .ts file

--- src/cap-angular-schematic-sfcore/files/account-sf/account-sf.component.ts
+++ src/cap-angular-schematic-sfcore/files/account-sf/account-sf.component.ts
@@ -2,13 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
-  templateUrl: './account-sf.component.html',
-  styleUrls: ['./account-sf.component.scss']
+  template: `
+  <section>
+    <app-account-sf></app-account-sf>
+  </section>
+    `,
+  styles: [`
+    section {
+      min-height: 100vh;
+      overflow: hidden;
+      width: 100%;
+      background-color: white;
+      transition: margin-left 4s ease-in-out 1s;
+      padding: 20px 20px;
+    }
+  `]
 })
 export class AccountSFComponent implements OnInit {
 
-  constructor() {}
-
-  ngOnInit() {}
+  constructor() { }
   
+  ngOnInit() { }
 }
